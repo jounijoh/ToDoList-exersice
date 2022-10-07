d81c2018267ae2c484b91981320debede8f4e5fd
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2022-10-07 10:20:26 +0300

    ag-grid tehtävä

--- src/components/TodoTable.js
+++ src/components/TodoTable.js
@@ -3,8 +3,7 @@ import React from "react";
 function TodoTable(props){
 
     const handleDelete = (index, e) => {
-        console.log(index)
-        console.log(props.todos)
+        
         props.setTodos(props.todos.filter((todo, i) => i !== index));
         
         
@@ -16,13 +15,13 @@ function TodoTable(props){
       <table>
         <thead>
           <tr>
-           <th> Description</th><th> Date</th>
+           <th> Description</th><th> Date</th><th>Priority</th>
           </tr>
         </thead>
         <tbody>
           {props.todos.map((todo, index) => (
             <tr key={index}>
-              <td>{todo.description}</td><td>{todo.date}</td>
+              <td>{todo.description}</td><td>{todo.date}</td><td>{todo.priority}</td>
               <td><button onClick={e => handleDelete(index, e)}> Delete </button></td>
             </tr>
           ))}
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,38 +1,54 @@
-import React, { useState } from "react";
-import TodoTable from "./TodoTable";
+import React, { useState, useRef } from "react";
+import { AgGridReact } from "ag-grid-react"; // the AG Grid React Component
+
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-material.css"; // Optional theme CSS
 
 function Todolist() {
   const [todo, setTodo] = useState({
     description: "",
-    date: ""
+    date: "",
+    priority: "",
   });
   const [todos, setTodos] = useState([]);
+  const gridRef = useRef();
+
+  const [columnDefs] = useState([
+    { field: "description", sortable: true, filter: true, floatingFilter: true },
+    { field: "date",sortable: true, filter: true, floatingFilter: true },
+    { field: "priority", sortable: true, filter: true,
+      cellStyle: params => params.value === "High" ? {color: "red"} : {color: "black"}, floatingFilter: true }
+  ]);
 
   const handleChange = (e) => {
     e.preventDefault();
     const value = e.target.value;
     setTodo({
       ...todo,
-      [e.target.name]: value
+      [e.target.name]: value,
     });
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTodos([todo, ...todos])
+    setTodos([todo, ...todos]);
     //reset input
-    setTodo({description: "", date: ""})
-    
+    setTodo({ description: "", date: "" , priority: ""});
+  };
+
+  const deleteTodo = () => {
+    if (gridRef.current.getSelectedNodes().length > 0)
+      setTodos(todos.filter((todo, index) => 
+      index !== gridRef.current.getSelectedNodes()[0].childIndex));
   }
-
-
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} >
         <label>
-          Description
+  
           <input
+            placeholder="Description"
             type="text"
             name="description"
             value={todo.description}
@@ -41,17 +57,41 @@ function Todolist() {
         </label>
 
         <label>
-          Date
+        
           <input
+            placeholder="Date"
             type="date"
             name="date"
             value={todo.date}
             onChange={handleChange}
           />
         </label>
+        <label>
+          
+          <input
+            placeholder="Priority"
+            type="text"
+            name="priority"
+            value={todo.priority}
+            onChange={handleChange}
+          />
+        </label>
+
         <input type="submit" value="Add ToDo" />
+        
       </form>
-    <TodoTable todos={todos} setTodos={setTodos}/>
+      <button onClick={deleteTodo}>Delete</button> 
+      
+      <div className="ag-theme-material" style={{margin: "auto", width: "60%", height: 600}}>
+        <AgGridReact 
+          ref={gridRef}
+          onGridReady= { params => gridRef.current = params.api }
+          rowSelection="single"
+          rowData={todos}
+          columnDefs={columnDefs}
+          animateRows={true}
+        />
+      </div>
     </div>
   );
 }
